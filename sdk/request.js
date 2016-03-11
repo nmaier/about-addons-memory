e7029e2fba5276bc013965e7f3fb67dceb861ab1
Nils Maier <maierman@web.de>
2016-03-11 16:14:26 +0100

    sdk/request module

---
+++ sdk/request.js
@@ -0,0 +1,101 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+Cu.importGlobalProperties(["XMLHttpRequest", "Blob", "File"]);
+
+exports.Blob = Blob;
+exports.File = File;
+exports.xhr = exports.XMLHttpRequest = XMLHttpRequest;
+
+function Callbacks(underlay) {
+  try {
+    this.underlay = underlay.QueryInterface(Ci.nsIInterfaceRequestor);
+  }
+  catch (ex) {
+    this.underlay = null;
+  }
+}
+Callbacks.prototype = {
+  badHost: null,
+  getInterface: function(iid) {
+    try {
+      return this.QueryInterface(iid);
+    }
+    catch (ex) {
+      if (this.underlay) {
+        return this.underlay.getInterface(iid);
+      }
+      throw ex;
+    }
+  },
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIBadCertListener2, Ci.nsIInterfaceRequestor]),
+  notifyCertProblem: function(si, status, host) {
+    this.badHost = host;
+    return false;
+  }
+};
+
+exports.request = (method, url, data, options) => {
+  options = options || {};
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    let cbs = null;
+    try {
+      url = url.spec || url;
+      if (method == "POST" && !data) {
+        let [u, ...pieces] = url.split("?");
+        url = u;
+        data = pieces.length && pieces.join("?") || null;
+      }
+      req.timeout = options.timeout || 10000;
+      if (options.mime) {
+        req.overrideMimeType(options.mime);
+      }
+      if (options.type) {
+        req.responseType = options.type;
+      }
+      else {
+        req.responseType = "json";
+      }
+      if (options.setup) {
+        options.setup(req);
+      }
+      req.addEventListener("loadend", function end() {
+        req.removeEventListener("loadend", end, false);
+        try {
+          if (req.status < 100 || req.status >= 400) {
+            if (cbs && cbs.badHost) {
+              throw new Error(`${cbs.badHost} has an invalid TLS Certificate`);
+            }
+            throw new Error("Bad response");
+          }
+          log(LOG_DEBUG, `request to ${url} resolved`);
+          resolve(req);
+        }
+        catch (ex) {
+          log(LOG_DEBUG, `request to ${url} rejected ${ex}`);
+          reject(ex);
+        }
+      }, false);
+      log(LOG_DEBUG, `Making request to ${url} w/ data ${data}`);
+      req.open(method, url);
+      if (req.channel) {
+        try {
+          cbs = new Callbacks(req.channel.notificationCallbacks);
+          req.channel.notificationCallbacks = cbs;
+        }
+        catch (ex) {
+          log(LOG_DEBUG, "Failed to set callbacks", ex);
+        }
+      }
+      req.send(data || null);
+    }
+    catch (ex) {
+      reject(ex);
+    }
+  });
+};
+
+/* vim: set et ts=2 sw=2 : */
